7936300421f6d4faf87bca7aaf6e6cc3d7cb806f
Tatsuo Indou <indou.tsystem@gmail.com>
2018-04-20 13:38:42 +0900

    update roles/common/files/setup.sh

--- roles/common/files/setup.sh
+++ roles/common/files/setup.sh
@@ -3,9 +3,11 @@
 
 cat <<EOT
 https://qiita.com/knqyf263/items/6865d0a61a23cb359476
+0. ifconfig, brctl show
 1. cp host_only.xml /etc/libvirt/qemu/networks
 2. virsh net-define host_only.xml
 3. virsh net-edit host_only
 4. virsh net-autostart host_only
 5. systemctl reboot
+6. ifconfig, brctl show
 EOT
